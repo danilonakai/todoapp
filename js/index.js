ad4924948a055905ec71cd2db638403ba8b8d448
Danilo Nakai <contact.danilonakai@gmail.com>
2021-01-15 04:40:03 +0900

    Layout improvements

--- js/index.js
+++ js/index.js
@@ -9,75 +9,107 @@ function create_list(list_name){
     
     lists.push(newList);
     listId++;
+
     update_view(id);
 }
+function delete_list(id){
+    
+    lists.splice(id,1);
 
+    if(lists.length > 0){
+        update_view(lists[0].id);
+        show_lists();
+    }else{
+        update_view("#");
+        show_lists();
+    }
+}
 function show_lists(){
     if(lists.length > 0){
         $("#lists").empty();
         $(lists).each(function(i){
             $("#lists").append('<li class="list" onclick="select_list(this)"><div><span id="'+lists[i].id+'">'+lists[i].list_name+'</span></div></li>');
         });
+    }else{
+        $("#lists").empty();
     }
 }
-
 function select_list(event){
     let id = $(event).find('span').attr('id');
 
     update_view(id);
 }
+function update_view(list_id){
+    let id;
 
-function add_item(list_id,item_value){
+    if(list_id == "#"){
+        $('.header').fadeOut();
+        $(".main-content .itens").empty();
+    }else{
+        $(lists).each(function(e){
+            if(lists[e].id == list_id){
+                id = e;
+            }
+        });
+
+        let list_name = lists[id].list_name;
+        
+        $('.header').fadeIn();
+        $('.main-content h1').html(list_name+'<i class="fas fa-trash-alt" onclick="delete_list('+id+')"></i>');
+        $('.main-content h1').attr('id',id);
+        show_itens(id);
+    }
+}
+function add_item(id,item_value){
     let item_id = itemId;
     let item_status = false;
     let newItem = {id: item_id, value: item_value, status: item_status}
     
-    lists[list_id].itens.push(newItem);
+    lists[id].itens.push(newItem);
     itemId++;
-    update_view(list_id);
+    update_view(lists[id].id);
 }
+function delete_item(list,item){
+    lists[list].itens.splice(item,1);
 
-function show_itens(list_id){
+    show_itens(list);
+}
+function show_itens(id){
     $(".main-content .itens").empty();
-    if(lists[list_id].itens.length > 0){
-        $(lists[list_id].itens).each(function(i){
-            let status = "";
-            let line_through = "";
-            if(lists[list_id].itens[i].status == true){status = "checked"; line_through = "style='text-decoration: line-through;'";}
-            $(".main-content .itens").append('<li><input type="checkbox" onchange="check_itens(event)" '+status+' id="'+lists[list_id].itens[i].id+'" class="'+lists[list_id].id+'" '+lists[list_id].itens[i].status+' /><span '+line_through+'>'+lists[list_id].itens[i].value+'</span></li>');
+
+    if(lists[id].itens.length > 0){
+        $(lists[id].itens).each(function(i){
+            let status;
+            let line_through;
+            if(lists[id].itens[i].status == true){status = "checked"; line_through = "style='text-decoration: line-through;'";}
+            $(".main-content .itens").append('<li><i class="fas fa-trash-alt" onclick="delete_item('+id+','+i+')"></i><input type="checkbox" onchange="check_itens(event)" '+status+' id="'+lists[id].itens[i].id+'" class="'+id+'" '+lists[id].itens[i].status+' /><span '+line_through+'>'+lists[id].itens[i].value+'</span></li>');
         });
     }
 }
-
 function check_itens(event){
-    let list_id = $(event)[0].className;
-    let item_id = $(event)[0].id;
-    let status = $(event)[0].checked;
+    let id = event.target.className;
+    let item_id = event.target.id;
+    let status = event.target.checked;
 
-    $(lists[list_id].itens).each(function(i){
-        if(lists[list_id].itens[i].id == item_id){
-            lists[list_id].itens[i].status = status;
+    console.log("tentando");
+
+    $(lists[id].itens).each(function(i){
+        if(lists[id].itens[i].id == item_id){
+            lists[id].itens[i].status = status;
         }
     });
 
-    show_itens(list_id);
+    console.log("consegui");
+
+    show_itens(id);
 }
-
-function update_view(list_id){
-    let list_name = lists[list_id].list_name;
-    $('.main-content h1').html(list_name);
-    $('.main-content h1').attr('id',list_id);
-
-    if($('.main-content h1').html().length > 0){
-        $('.add-item').css('display','block');
-    }
-    
-    show_itens(list_id);
-}
-
 function modal_animation(action){
     if(action == "open"){
-
+        $('.modal-bg').fadeIn(400,function(){
+            $("#modal").fadeIn(400,function(){
+                $("#list-name").focus();
+            });
+        });
     }
     else if(action == "close"){
         $('#modal').fadeOut(200,function(){
@@ -86,19 +118,47 @@ function modal_animation(action){
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
-
-    // ABRE O MODAL PARA CRIAR A LISTA
+    //CREATE LIST
     $('#add-list').click(function(){
-        $('.modal-bg').fadeIn(400,function(){
-            $("#modal").fadeIn(400,function(){
-                $("#list-name").focus();
-            });
-        });
+        modal_animation("open");
     }); 
+    $('#btn-create-list').click(function(e){
+        e.preventDefault();
 
-    // CRIA UMA NOVA LISTA
-    $('#btn-create-list').click(function(){
         let list_name = $("#list-name").val();
 
         create_list(list_name);
@@ -109,18 +169,19 @@ $(document).ready(function(){
         
         show_lists();
     });
-
-    // CLOSE MODAL
     $('.modal-bg').click(function(){
         modal_animation("close");
     });
 
-    // CRIA UM NOVO ITEM NA LISTA ATUAL
-    $('#btn-add-item').click(function(){
-        var list_id = $('.main-content h1').attr("id");
+
+
+    //ADD ITEM
+    $('#btn-add-item').click(function(e){
+        e.preventDefault();
+        var id = $('.main-content h1').attr("id");
         var item_value = $('#item-value').val();
         
-        add_item(list_id,item_value,);
+        add_item(id,item_value,);
         
         $('#item-value').val("");
     });
