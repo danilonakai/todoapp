d2611517f20a1642864194acc2d7b25f3688589d
中井ヒロシ <contact@danilonakai.com>
2022-03-29 19:13:16 +0900

    Started creating add_item_to_list function

--- src/components/Sample/Sample.jsx
+++ src/components/Sample/Sample.jsx
@@ -8,7 +8,8 @@ export default class Sample extends Component{
         super(props);
         
         this.state = {
-            id: 1,
+            list_id: 1,
+            item_id: 1,
             data: {
                 lists: []
             },
@@ -21,7 +22,7 @@ export default class Sample extends Component{
     }
 
     create_list(title){
-        let id = this.state.id;
+        let id = this.state.list_id;
         let data = this.state.data;
         let new_data = JSON.parse(JSON.stringify(data));
         
@@ -33,7 +34,7 @@ export default class Sample extends Component{
 
         this.setState({
             data: new_data,
-            id: id+1
+            list_id: id+1
         },()=> console.log(this.state.data));
     }
     
@@ -61,13 +62,34 @@ export default class Sample extends Component{
             if(window.confirm("Are you sure you want to delete?")){
                 this.setState({
                     data: new_data
-                }, () => alert("List deleted!"));
+                }, () => {
+                    if(this.state.selected_list.id === id){
+                        this.setState({
+                            selected_list: null
+                        });
+                    }
+
+                    alert("List deleted!");
+                });
             }
         }        
     }
 
     add_item_to_list(list_id,item_title){
+        let id = this.state.item_id;
+        let data = this.state.data;
+        let new_data = JSON.parse(JSON.stringify(data));
+        new_data = new_data.lists.filter(list => list.id === list_id)[0].items.push({
+            id: id,
+            title: item_title,
+            checked: false
+        });
 
+
+        new_data = { lists:  new_data};
+
+        console.log(data);
+        console.log(new_data);
     }
 
     delete_item_from_list(list_id,item_id){
@@ -85,11 +107,19 @@ export default class Sample extends Component{
             <header className="App-header">
                 <button onClick={() => console.log(this.state.data)}>show lists</button>
 
+                <button onClick={() => console.log(this.state.selected_list)}>show selected list</button>
+
                 <button onClick={()=> this.create_list("test")}>create list</button>
 
                 <button onClick={() => this.select_list(3)}>select 3rd list</button>
 
                 <button onClick={() => this.delete_list(3)}>delete 3rd list</button>
+
+                <button onClick={() => this.add_item_to_list(3,"item title")}>add item to 3rd list</button>
+
+                <button onClick={() => this.delete_item_from_list(3,3)}>delete 3rd item from 3rd list</button>
+
+                <button onClick={() => this.check_item(3,3)}>check 3rd item from 3rd list</button>
             </header>
         )
     }
